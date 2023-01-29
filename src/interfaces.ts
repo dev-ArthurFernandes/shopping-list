bec6afea290662f63609f54ca5b43b172e49ace6
Arthur Fernandes <arthurdesousafernandes@gmail.com>
2023-01-29 12:56:53 -0300

    feat: Create Middlewares

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -14,4 +14,6 @@ interface iData{
 
 type ShopListRequiredKeys = "listName" | "data"
 
-export {iShopListRequest, iShopList, iData, ShopListRequiredKeys}
+type dataRequiredKeys = "name" | "quantity"
+
+export {iShopListRequest, iShopList, iData, ShopListRequiredKeys, dataRequiredKeys}
